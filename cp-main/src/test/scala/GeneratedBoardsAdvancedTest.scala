80ba1ea9afd9f664c5f7005d9bfa638e7fe81264
Arthur S <kermitas@gmail.com>
2014-02-12 15:18:20 +0100

    Splitted into basic tests and advanced tests.
    Added more tests.

---
+++ cp-main/src/test/scala/GeneratedBoardsAdvancedTest.scala
@@ -0,0 +1,25 @@
+import org.scalatest._
+
+import as.chess.problem.piece._
+import as.chess.problem.board.{ BoardGenerator, Board }
+
+class GeneratedBoardsAdvancedTest extends FeatureSpec with GivenWhenThen with Matchers {
+
+  final val runAdvancedTests = false
+
+  if (runAdvancedTests) {
+
+    scenario("scenario: 8x8 board 8x Queen") {
+      Given("board (8,8)")
+      val board = new Board(8, 8)
+      And("8x Queen")
+      val piecesStream = List[Piece](Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen).toStream
+      When("we generate boards")
+      val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+      Then("result should be 12 unique board")
+      boardsStream.filter(_.isDefined).size should be(12)
+    }
+
+  }
+
+}
--- cp-main/src/test/scala/GeneratedBoardsTest.scala
+++ cp-main/src/test/scala/GeneratedBoardsTest.scala
@@ -60,11 +60,66 @@ class GeneratedBoardsTest extends FeatureSpec with GivenWhenThen with Matchers {
     boardsStream.filter(_.isDefined).size should be(1)
   }
 
+  scenario("scenario: 3x3 board 2x King, 1x Rook (first repetition of previous one but with other pieces order)") {
+    Given("board (3,3)")
+    val board = new Board(3, 3)
+    And("2x King, 1x Rook")
+    val piecesStream = List[Piece](King.king, Rook.rook, King.king).toStream
+    When("we generate boards")
+    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+    Then("result should be 1 unique board")
+    boardsStream.filter(_.isDefined).size should be(1)
+  }
+
+  scenario("scenario: 3x3 board 2x King, 1x Rook (second repetition of previous one but with other pieces order)") {
+    Given("board (3,3)")
+    val board = new Board(3, 3)
+    And("2x King, 1x Rook")
+    val piecesStream = List[Piece](Rook.rook, King.king, King.king).toStream
+    When("we generate boards")
+    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+    Then("result should be 1 unique board")
+    boardsStream.filter(_.isDefined).size should be(1)
+  }
+
   scenario("scenario: 4x4 board 4x Knight, 2x Rook") {
     Given("board (4,4)")
     val board = new Board(4, 4)
     And("4x Knight, 2x Rook")
     val piecesStream = List[Piece](Knight.knight, Knight.knight, Knight.knight, Knight.knight, Rook.rook, Rook.rook).toStream
+    When("we generate boards")
+    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+    Then("result should be 2 unique boards")
+    boardsStream.filter(_.isDefined).size should be(2)
+  }
+
+  scenario("scenario: 4x4 board 4x Knight, 2x Rook (first repetition of previous one but with other pieces order)") {
+    Given("board (4,4)")
+    val board = new Board(4, 4)
+    And("4x Knight, 2x Rook")
+    val piecesStream = List[Piece](Rook.rook, Knight.knight, Knight.knight, Rook.rook, Knight.knight, Knight.knight).toStream
+    When("we generate boards")
+    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+    Then("result should be 2 unique boards")
+    boardsStream.filter(_.isDefined).size should be(2)
+  }
+
+  scenario("scenario: 4x4 board 4x Knight, 2x Rook (second repetition of previous one but with other pieces order)") {
+    Given("board (4,4)")
+    val board = new Board(4, 4)
+    And("4x Knight, 2x Rook")
+    val piecesStream = List[Piece](Rook.rook, Rook.rook, Knight.knight, Knight.knight, Knight.knight, Knight.knight).toStream
+    When("we generate boards")
+    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
+    Then("result should be 2 unique boards")
+    boardsStream.filter(_.isDefined).size should be(2)
+  }
+
+  scenario("scenario: 4x4 board 4x Knight, 2x Rook (third repetition of previous one but with other pieces order)") {
+    Given("board (4,4)")
+    val board = new Board(4, 4)
+    And("4x Knight, 2x Rook")
+    val piecesStream = List[Piece](Knight.knight, Knight.knight, Rook.rook, Rook.rook, Knight.knight, Knight.knight).toStream
     When("we generate boards")
     val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
     Then("result should be 2 unique boards")
@@ -113,17 +168,6 @@ class GeneratedBoardsTest extends FeatureSpec with GivenWhenThen with Matchers {
     val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
     Then("result should be 6 unique board")
     boardsStream.filter(_.isDefined).size should be(6)
-  }
-
-  scenario("scenario: 8x8 board 8x Queen") {
-    Given("board (8,8)")
-    val board = new Board(8, 8)
-    And("8x Queen")
-    val piecesStream = List[Piece](Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen, Queen.queen).toStream
-    When("we generate boards")
-    val boardsStream = BoardGenerator.generateBoardsStream(board, piecesStream)
-    Then("result should be 12 unique board")
-    boardsStream.filter(_.isDefined).size should be(12)
   }
 
 }
